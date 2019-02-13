4a94b5fe2e122904c2a2bc765c4111d1a73796b9
Dr Nic Williams <drnicwilliams@gmail.com>
2019-02-13 11:44:41 +1000

    initial dronekit/mavlink connection

---
+++ python/01_basic_connection.py
@@ -0,0 +1,14 @@
+from dronekit import connect, VehicleMode
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--connect',
+                    help='Connection string to MAVLink',
+                    default='udp:127.0.0.1:14550')
+args = parser.parse_args()
+
+print(">>>> Connecting with the UAV <<<")
+# vehicle = connect(connection_string, wait_ready=True)     #- wait_ready flag hold the program untill all the parameters are been read (=, not .)
+vehicle = connect(args.connect)
+
---
+++ python/README.md
@@ -0,0 +1,8 @@
+# MAVLink Playtime in Python
+
+The code examples assume Python 3+. To install dependencies:
+
+```shell
+pip3 install -r python/requirements.txt
+```
+
